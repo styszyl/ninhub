6e7d8cbfd910ae9c567f3d07f2c9506ae28faf19
styszyl <97724595+styszyl@users.noreply.github.com>
2023-11-11 22:17:20 +0100

    changed descriptions for elements, adjusted values

--- src/constants.js
+++ src/constants.js
@@ -1,71 +1,89 @@
 export const jsQuizz = {
     masteries: [
-      {fire: 0, description:"(fire description)"},
-      {wind: 0, description:"(wind description)"},
-      {earth: 0, description:"(earth description)"},
-      {water: 0, description:"(water description)"},
-      {medic: 0, description:"(medic description)"},
-      {str_wm: 0, description:"(STR wm description)"},
-      {int_wm: 0, description:"(INT wm description)"},
-      {taijutsu: 0, description:"(taijutsu description)"},
-      {light: 0, description:"(light description)"},
-      {fan: 0, description:"(fan description)"},
-      {bubble: 0, description:"(bubble description)"},
-      {gf: 0, description:"(gf description)"},
+      {Fire: 0, description:"(Fire description)"},
+      {Wind: 0, description:"(Wind description)"},
+      {Earth: 0, description:"(Earth description)"},
+      {Water: 0, description:"(Water description)"},
+      {Medic: 0, description:"(Medic description)"},
+      {STR_WM: 0, description:"(STR WM description)"},
+      {INT_WM: 0, description:"(INT WM description)"},
+      {Taijutsu: 0, description:"(Taijutsu description)"},
+      {Lightning: 0, description:"(Lightning description)"},
+      {Fan: 0, description:"(Fan description)"},
+      {Bubble: 0, description:"(Bubble description)"},
+      {Gentle_fist: 0, description:"(Gentle Fist description)"},
     ],
     questions: [
       {
         question:
           "What do you enjoy the most in MMORPG?",
         choices: [
-          {text: "AOE PVE (grind)", value: {fire: 5, water: 3, wind: 2, earth: 3, int_wm: 3, bubble: 3, fan: 2, gf: 1, light: 1}},
-          {text: "SINGLE TARGET PVE (boss farming)", value: {fire: 5, water: 2, wind: 3, earth: 2, int_wm: 4, bubble: 2, fan: 3, light: 3}},
-          {text: "SOLO PVP", value: {str_wm: 5, wm_int: 5, fire: 4, taijutsu: 5, wind: 4, earth: 2, water: 5, light: 3, fan: 5, bubble: 4, gf: 5}},
-          {text: "GROUP PVP", value: {wm_int: 5, fire: 4, taijutsu: 1, wind: 2, earth: 5, water: 2, medic: 5, light: 5, fan: 2, bubble: 4}},
-          {text: "Both PVE and PVP", value: {fire: 3, wind: 3, earth: 3, water: 3, medic: 3, str_wm: 3, int_wm: 3, taijutsu: 3, light: 3, fan: 3, bubble: 3, gf: 3}},
+          {text: "AOE PVE (grind)", value: {Fire: 5, Water: 3, Wind: 2, Earth: 3, INT_WM: 3, Bubble: 3, Fan: 2, Gentle_fist: 1, Lightning: 1}},
+          {text: "SINGLE TARGET PVE (boss farming)", value: {Fire: 5, Water: 2, Wind: 3, Earth: 2, INT_WM: 4, Bubble: 2, Fan: 3, Lightning: 3}},
+          {text: "SOLO PVP", value: {STR_WM: 5, wm_int: 5, Fire: 4, Taijutsu: 5, Wind: 4, Earth: 2, Water: 5, Lightning: 3, Fan: 5, Bubble: 4, Gentle_fist: 5}},
+          {text: "GROUP PVP", value: {wm_int: 5, Fire: 4, Taijutsu: 1, Wind: 2, Earth: 5, Water: 2, Medic: 5, Lightning: 5, Fan: 2, Bubble: 4}},
+          {text: "Both PVE and PVP", value: {Fire: 3, Wind: 3, Earth: 3, Water: 3, Medic: 3, STR_WM: 3, INT_WM: 3, Taijutsu: 3, Lightning: 3, Fan: 3, Bubble: 3, Gentle_fist: 3}},
         ],
       },
       {
         question:
           "What role do you prefer?",
         choices: [
-          {text: "Damage Dealer (high burst)", value: {fire: 5, wind: 5, earth: 3, water: 3, medic: 2, str_wm: 2, int_wm: 5, taijutsu: 1, light: 4, fan: 2, bubble: 1, gf: 2}},
-          {text: "Damage Dealer (high DPS)", value: {fire: 3, wind: 1, earth: 1, water: 1, medic: 2, str_wm: 5, int_wm: 2, taijutsu: 5, light: 2, fan: 3, bubble: 2, gf: 1}},
-          {text: "Crowd control", value: {fire: 2, wind: 1, earth: 5, water: 3, medic: 1, str_wm: 3, int_wm: 1, taijutsu: 2, light: 5, fan: 4, bubble: 5, gf: 3}},
-          {text: "Support", value: {fire: 0, wind: 1, earth: 3, water: 0, medic: 5, str_wm: 0, int_wm: 2, taijutsu: 2, light: 3, fan: 2, bubble: 3, gf: 2}},
+          {text: "Damage Dealer (high burst)", value: {Fire: 5, Wind: 5, Earth: 3, Water: 3, Medic: 2, STR_WM: 2, INT_WM: 5, Taijutsu: 1, Lightning: 4, Fan: 2, Bubble: 1, Gentle_fist: 2}},
+          {text: "Damage Dealer (high DPS)", value: {Fire: 3, Wind: 1, Earth: 1, Water: 1, Medic: 2, STR_WM: 5, INT_WM: 2, Taijutsu: 5, Lightning: 2, Fan: 3, Bubble: 2, Gentle_fist: 1}},
+          {text: "Crowd control", value: {Fire: 2, Wind: 1, Earth: 5, Water: 3, Medic: 1, STR_WM: 3, INT_WM: 1, Taijutsu: 2, Lightning: 5, Fan: 4, Bubble: 5, Gentle_fist: 3}},
+          {text: "Support", value: {Fire: 0, Wind: 1, Earth: 3, Water: 0, Medic: 5, STR_WM: 0, INT_WM: 2, Taijutsu: 2, Lightning: 3, Fan: 2, Bubble: 3, Gentle_fist: 2}},
         ],
       },
       {
         question:
+          "Are you more interested in masteries that excel in controlling the battlefield(traps, Firewall, Earthwall) or controlling movement of your enemies(stun, snare, slow, cancel)? ",
+        choices: [
+          {text: "I am interested in controlling the battlefield", value: {Fire: 2, Wind: 0, Earth: 3, Water: 0, Medic: 0, STR_WM: 0, INT_WM: 5, Taijutsu: 0, Lightning: 0, Fan: 0, Bubble: 0, Gentle_fist: 0}},
+          {text: "I am interested in controlling my enemies", value: {Fire: 2, Wind: 1, Earth: 3, Water: 2, Medic: 1, STR_WM: 2, INT_WM: 1, Taijutsu: 3, Lightning: 5, Fan: 3, Bubble: 5, Gentle_fist: 3}},
+        ],
+      },
+
+      {
+        question:
           "In combat, where are you, and what are you doing?",
         choices: [
-          {text: "I try to stand back and support my teammates.", value: {fire: 2, wind: 1, earth: 4, water: 2, medic: 4, str_wm: 0, int_wm: 3, taijutsu: 0, light: 4, fan: 1, bubble: 4, gf: 1}},
-          {text: "I am healing!", value: {fire: 0, wind: 0, earth: 0, water: 0, medic: 5, str_wm: 0, int_wm: 0, taijutsu: 0, light: 0, fan: 0, bubble: 0, gf: 0}},
-          {text: "I am waiting for perfect opportunity to kill my enemies in single strike from stealth", value: {fire: 3, wind: 2, earth: 1, water: 2, medic: 1, str_wm: 0, int_wm: 3, taijutsu: 0, light: 1, fan: 1, bubble: 0, gf: 0}},
-          {text: "I am waiting for perfect set up from my teammates", value: {fire: 4, wind: 4, earth: 3, water: 2, medic: 3, str_wm: 2, int_wm: 4, taijutsu: 2, light: 4, fan: 2, bubble: 3, gf: 2}},
-          {text: "I am always on frontline disrupting my enemies", value: {fire: 1, wind: 1, earth: 1, water: 3, medic: 1, str_wm: 4, int_wm: 1, taijutsu: 5, light: 1, fan: 5, bubble: 3, gf: 4}},
-          {text: "One man army", value: {fire: 0, wind: 0, earth: 0, water: 0, medic: 0, str_wm: 5, int_wm: 0, taijutsu: 5, light: 0, fan: 5, bubble: 4, gf: 4}},
-          {text: "I set up enemies for my teammates", value: {fire: 1, wind: 1, earth: 5, water: 3, medic: 1, str_wm: 3, int_wm: 1, taijutsu: 2, light: 5, fan: 3, bubble: 5, gf: 2}},
+          {text: "I try to stand back and support my teammates.", value: {Fire: 2, Wind: 1, Earth: 4, Water: 2, Medic: 4, STR_WM: 0, INT_WM: 3, Taijutsu: 0, Lightning: 4, Fan: 1, Bubble: 4, Gentle_fist: 1}},
+          {text: "I am healing!", value: {Fire: 0, Wind: 0, Earth: 0, Water: 0, Medic: 5, STR_WM: 0, INT_WM: 0, Taijutsu: 0, Lightning: 0, Fan: 0, Bubble: 0, Gentle_fist: 0}},
+          {text: "I am waiting for perfect opportunity to kill my enemies in single strike from stealth", value: {Fire: 3, Wind: 2, Earth: 1, Water: 2, Medic: 1, STR_WM: 0, INT_WM: 3, Taijutsu: 0, Lightning: 1, Fan: 1, Bubble: 0, Gentle_fist: 0}},
+          {text: "I am waiting for perfect set up from my teammates", value: {Fire: 4, Wind: 4, Earth: 3, Water: 2, Medic: 3, STR_WM: 2, INT_WM: 4, Taijutsu: 2, Lightning: 4, Fan: 2, Bubble: 3, Gentle_fist: 2}},
+          {text: "I am always on frontline disrupting my enemies", value: {Fire: 1, Wind: 1, Earth: 1, Water: 3, Medic: 1, STR_WM: 4, INT_WM: 1, Taijutsu: 5, Lightning: 1, Fan: 5, Bubble: 3, Gentle_fist: 4}},
+          {text: "One man army", value: {Fire: 0, Wind: 0, Earth: 0, Water: 0, Medic: 0, STR_WM: 5, INT_WM: 0, Taijutsu: 5, Lightning: 0, Fan: 5, Bubble: 4, Gentle_fist: 4}},
+          {text: "I set up enemies for my teammates", value: {Fire: 1, Wind: 1, Earth: 5, Water: 3, Medic: 1, STR_WM: 3, INT_WM: 1, Taijutsu: 2, Lightning: 5, Fan: 3, Bubble: 5, Gentle_fist: 2}},
         ],
       },
       {
         question:
           "During battle do you prefer to play safe or do you take risk?",
         choices: [
-          {text: "I take risk!", value: {fire: 2, wind: 3, earth: 3, water: 1, medic: 0, str_wm: 1, int_wm: 1, taijutsu: 1, light: 4, fan: 0, bubble: 0, gf: 2}},
-          {text: "I play safe!", value: {fire: 0, wind: 0, earth: 0, water: 4, medic: 5, str_wm: 3, int_wm: 4, taijutsu: 4, light: 2, fan: 4, bubble: 5, gf: 3}},
+          {text: "I take risk!", value: {Fire: 3, Wind: 2, Earth: 2, Water: 1, Medic: 0, STR_WM: 1, INT_WM: 1, Taijutsu: 1, Lightning: 4, Fan: 1, Bubble: 0, Gentle_fist: 2}},
+          {text: "I play safe!", value: {Fire: 0, Wind: 1, Earth: 1, Water: 5, Medic: 4, STR_WM: 3, INT_WM: 4, Taijutsu: 3, Lightning: 0, Fan: 4, Bubble: 5, Gentle_fist: 3}},
+        ],
+      },
+      {
+        question:
+          "In PvP situations, do you prefer one-on-one duels, small skirmishes, or large-scale battlegrounds?",
+        choices: [
+          {text: "One-on-one duels", value: {Fire: 3, Wind: 4, Earth: 2, Water: 5, Medic: 2, STR_WM: 5, INT_WM: 2, Taijutsu: 5, Lightning: 1, Fan: 5, Bubble: 4, Gentle_fist: 5}},
+          {text: "Small skirmishes", value: {Fire: 0, Wind: 0, Earth: 0, Water: 4, Medic: 5, STR_WM: 3, INT_WM: 4, Taijutsu: 4, Lightning: 2, Fan: 4, Bubble: 5, Gentle_fist: 3}},
+          {text: "Large-scale battlegrounds", value: {Fire: 2, Wind: 3, Earth: 3, Water: 1, Medic: 0, STR_WM: 1, INT_WM: 1, Taijutsu: 1, Lightning: 4, Fan: 0, Bubble: 4, Gentle_fist: 2}},
         ],
       },
       {
         question:
           "Which weapon(s) would you prefer?",
         choices: [
-          {text: "Swords - (STR weapons)", value: {fire: 0, wind: 0, earth: 0, water: 0, medic: 0, str_wm: 10, int_wm: 0, taijutsu: 0, light: 0, fan: 0, bubble: 0, gf: 6}},
-          {text: "Hand-to-hand combat (STR/AGI/CHK stat)", value: {fire: 0, wind: 0, earth: 0, water: 0, medic: 6, str_wm: 0, int_wm: 0, taijutsu: 10, light: 0, fan: 0, bubble: 0, gf: 8}},
-          {text: "Fans - (STR ranged weapons)", value: {fire: 0, wind: 0, earth: 0, water: 0, medic: 0, str_wm: 0, int_wm: 0, taijutsu: 0, light: 0, fan: 10, bubble: 0, gf: 0}},
-          {text: "Kunais/staffs - (INT weapons)", value: {fire: 5, wind: 5, earth: 5, water: 5, medic: 5, str_wm: 0, int_wm: 5, taijutsu: 0, light: 5, fan: 0, bubble: 0, gf: 0}},
-          {text: "Bubble pipes - (STR snare weapons)", value: {fire: 0, wind: 0, earth: 0, water: 0, medic: 0, str_wm: 0, int_wm: 0, taijutsu: 0, light: 0, fan: 0, bubble: 10, gf: 0}},
-          {text: "Bubble pipes - (INT HYBRID snare weapons)", value: {fire: 0, wind: 0, earth: 0, water: 10, medic: 0, str_wm: 0, int_wm: 0, taijutsu: 0, light: 0, fan: 0, bubble: 0, gf: 0}}
+          {text: "Swords - (STR weapons)", value: {Fire: 0, Wind: 0, Earth: 0, Water: 0, Medic: 0, STR_WM: 10, INT_WM: -10, Taijutsu: -10, Lightning: 0, Fan: 0, Bubble: 0, Gentle_fist: 6}},
+          {text: "Hand-to-hand combat (STR/AGI/CHK stat)", value: {Fire: 0, Wind: 0, Earth: 0, Water: 0, Medic: 6, STR_WM: 4, INT_WM: 0, Taijutsu: 10, Lightning: 0, Fan: -5, Bubble: -5, Gentle_fist: 8}},
+          {text: "Fans - (STR ranged weapons)", value: {Fire: 0, Wind: -10, Earth: 0, Water: 0, Medic: 0, STR_WM: 0, INT_WM: 0, Taijutsu: -10, Lightning: 0, Fan: 10, Bubble: -10, Gentle_fist: -10}},
+          {text: "Kunais/staffs - (INT weapons)", value: {Fire: 5, Wind: 5, Earth: 5, Water: 5, Medic: 5, STR_WM: 0, INT_WM: 5, Taijutsu: -5, Lightning: 5, Fan: -10, Bubble: -10, Gentle_fist: -10}},
+          {text: "Bubble pipes - (STR snare weapons)", value: {Fire: 0, Wind: 0, Earth: 0, Water: -10, Medic: 0, STR_WM: 5, INT_WM: 0, Taijutsu: 0, Lightning: 0, Fan: -10, Bubble: 10, Gentle_fist: -10}},
+          {text: "Bubble pipes - (INT HYBRID snare weapons)", value: {Fire: 0, Wind: 0, Earth: 0, Water: 10, Medic: 0, STR_WM: -5, INT_WM: 0, Taijutsu: -10, Lightning: 0, Fan: -10, Bubble: -10, Gentle_fist: -10}}
         ],
       },
     ],
