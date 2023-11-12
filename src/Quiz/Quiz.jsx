e00ea9d268e616a21351e0ceebbba0b9c62cde26
styszyl <97724595+styszyl@users.noreply.github.com>
2023-11-12 22:28:07 +0100

    added RadarChar in quiz component

--- src/Quiz/Quiz.jsx
+++ src/Quiz/Quiz.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./Quiz.css";
+import RadarChart from "../radarChart/radarChart";
 
 const Quiz = ({ questions, masteries }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -7,7 +8,7 @@ const Quiz = ({ questions, masteries }) => {
   const [showResult, setShowResult] = useState(false);
 
   const { question, choices } = questions[currentQuestion];
-  console.log(masteries);
+  // console.log(masteries);
   const ChangeCurrentQuestion = () => {
     if (currentQuestion !== questions.length - 1) {
       setCurrentQuestion(currentQuestion + 1);
@@ -44,24 +45,17 @@ const Quiz = ({ questions, masteries }) => {
   const getMasteryCombinations = () => {
     const topMasteries = getTopMasteries();
     const combinations = [];
-    const descriptions = [];
 
     const firstMastery = Object.keys(topMasteries[0])[0];
     const secondMastery = Object.keys(topMasteries[1])[0];
-    const firstDescription = Object.values(topMasteries[0])[1];
-    const secondDescription = Object.values(topMasteries[1])[1];
-
 
     combinations.push(`${firstMastery} and ${secondMastery}`);
-    descriptions.push(`${firstDescription} and ${secondDescription}`)
     for (let i = 2; i < masteries.length; i++) {
       const currentMastery = Object.keys(masteries[i])[0];
       if (Object.values(masteries[i])[0] === Object.values(masteries[1])[0]) {
         combinations.push(`${firstMastery} and ${currentMastery}`);
       }
     }
-    
-
     return combinations;
   };
   const getMasteryCombinationsDescriptions = () => {
@@ -75,13 +69,93 @@ const Quiz = ({ questions, masteries }) => {
     for (let i = 2; i < masteries.length; i++) {
       const currentMasteryDescription = Object.values(masteries[i])[1];
       if (Object.values(masteries[i])[0] === Object.values(masteries[1])[0]) {
-        // descriptions.push(`${firstDescription}`);
         descriptions.push(`${currentMasteryDescription}`);
       }
     }
-    
-    console.log(descriptions)
+
+    // console.log(descriptions);
     return descriptions;
+  };
+
+  const getChartData = () => {
+    const topMasteries = getTopMasteries();
+    const chartsData = [];
+    const firstMasteryChartData = Object.values(topMasteries[0])[2];
+    const secondMasteryChartData = Object.values(topMasteries[1])[2];
+
+    if (firstMasteryChartData && typeof firstMasteryChartData === "object") {
+      const firstChartDataList = {
+        label: Object.keys(topMasteries[0])[0], // Etykieta zestawu danych, widoczna w legendzie
+        data: [], // Tablica wartości punktów w zestawie danych
+        backgroundColor: "rgba(252, 147, 47, 0.7)", // Kolor wypełnienia obszaru pod linią
+        borderColor: "rgba(252, 147, 47, 0.9)", // Kolor obramowania linii
+        borderWidth: 1, // Grubość obramowania linii
+        pointBorderWidth: 2, // Grubość obramowania punktów
+        pointRadius: 2, // Rozmiar punktów
+      };
+      for (const key in firstMasteryChartData) {
+        if (firstMasteryChartData.hasOwnProperty(key)) {
+          const value = firstMasteryChartData[key];
+          // console.log(`First mastery ${key}, ${value}`);
+          firstChartDataList.data.push(value);
+        }
+      }
+      chartsData.push(firstChartDataList);
+    } else {
+      console.error("firstMasteryChartData is not an object");
+      // Możesz zwrócić pustą tablicę lub inny sposób obsługi błędu
+    }
+
+    if (secondMasteryChartData && typeof secondMasteryChartData === "object") {
+      const secondChartDataList = {
+        label: Object.keys(topMasteries[1])[0], // Etykieta zestawu danych, widoczna w legendzie
+        data: [], // Tablica wartości punktów w zestawie danych
+        backgroundColor: "rgba(124, 0, 195, 0.77)", // Kolor wypełnienia obszaru pod linią
+        borderColor: "rgba(124, 0, 195, 1)", // Kolor obramowania linii
+        borderWidth: 1, // Grubość obramowania linii
+        pointBorderWidth: 2, // Grubość obramowania punktów
+        pointRadius: 2, // Rozmiar punktów
+      };
+      for (const key in secondMasteryChartData) {
+        if (secondMasteryChartData.hasOwnProperty(key)) {
+          const value = secondMasteryChartData[key];
+          // console.log(`Second mastery ${key}, ${value}`);
+          secondChartDataList.data.push(value);
+        }
+      }
+      chartsData.push(secondChartDataList);
+    } else {
+      console.error("secondMasteryChartData is not an object");
+    }
+    for (let i = 2; i < masteries.length; i++) {
+      if (Object.values(masteries[i])[0] === Object.values(masteries[1])[0]) {
+        const currentMasteryDataChartList = [];
+        const currentMasteryDataChart = Object.values(masteries[i])[2];
+        const restMasteryDataChart = {
+          label: Object.keys(masteries[i])[0], // Etykieta zestawu danych, widoczna w legendzie
+          data: [], // Tablica wartości punktów w zestawie danych
+          backgroundColor: "rgba(32, 228, 63, 0.6)", // Kolor wypełnienia obszaru pod linią
+          borderColor: "rgba(32, 228, 63, 1)", // Kolor obramowania linii
+          borderWidth: 1, // Grubość obramowania linii
+          pointBorderWidth: 2, // Grubość obramowania punktów
+          pointRadius: 2, // Rozmiar punktów
+        };
+        for (const key in currentMasteryDataChart) {
+          if (currentMasteryDataChart.hasOwnProperty(key)) {
+            const value = currentMasteryDataChart[key];
+            currentMasteryDataChartList.push(value);
+            
+          } else {
+            console.log("error")
+          }
+        }
+        restMasteryDataChart.data = currentMasteryDataChartList;
+        chartsData.push(restMasteryDataChart);
+      }
+    }
+
+    console.log(chartsData);
+    return chartsData;
   };
 
   return (
@@ -117,6 +191,10 @@ const Quiz = ({ questions, masteries }) => {
                 <p>{combination}</p>
               </div>
             ))}
+            <RadarChart
+              datasets={getChartData()}
+              options={RadarChart.options}
+            />
             {getMasteryCombinationsDescriptions().map((description, index) => (
               <div key={index} className="quiz__combinations">
                 <p>{description}</p>
