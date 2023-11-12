afb57ab28a8821bcd661cb10325f89153e8f7f79
styszyl <97724595+styszyl@users.noreply.github.com>
2023-11-12 22:27:03 +0100

    formated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import Quiz from "./Quiz/Quiz";
-import { jsQuizz } from "./constants"
+import { jsQuizz } from "./constants";
 
 function App() {
-  return <Quiz masteries={jsQuizz.masteries} questions={jsQuizz.questions} />
+  return (
+    <div>
+      <h1>Quiz App</h1>
+      <Quiz masteries={jsQuizz.masteries} questions={jsQuizz.questions} />
+    </div>
+  );
 }
 
-export default App
+export default App;
