e3fb5f5b6145e23a8d169022ac5525c5f0d3bb10
styszyl <97724595+styszyl@users.noreply.github.com>
2023-11-13 22:50:50 +0100

    deleted h1 "Quiz App"

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { jsQuizz } from "./constants";
 function App() {
   return (
     <div>
-      <h1>Quiz App</h1>
       <Quiz masteries={jsQuizz.masteries} questions={jsQuizz.questions} />
     </div>
   );
